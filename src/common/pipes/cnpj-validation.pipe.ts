27696642f98c5c2c616ba6d605daaeff79ac4d88
William Fernandes Dias <william.winchester1967@gmail.com>
2023-11-20 16:41:39 -0300

    fix: include documentation and remove punctuation after validation (only the digits are registered in the database)

--- src/common/pipes/cnpj-validation.pipe.ts
+++ src/common/pipes/cnpj-validation.pipe.ts
@@ -1,12 +1,22 @@
 import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 import { cnpj } from 'cpf-cnpj-validator';
 
+/**
+ * CNPJ validation pipe.
+ *
+ * It verifies if the given CNPJ contains 14 digits and if it's valid.
+ * If not, it throws an `BadRequestException`.
+ *
+ * @exports
+ * @class CnpjValidationPipe
+ * @implements {PipeTransform}
+ */
 @Injectable()
 export class CnpjValidationPipe implements PipeTransform<string> {
   transform(value: string) {
     if (!value || !cnpj.isValid(value)) {
-      throw new BadRequestException('Invalid CNPJ');
+      throw new BadRequestException('Invalid CNPJ.');
     }
-    return value;
+    return cnpj.strip(value);
   }
 }
