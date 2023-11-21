0c916d4a036f6a4b5567d48312d28456622b6d86
William Fernandes Dias <william.winchester1967@gmail.com>
2023-11-20 22:42:21 -0300

    fix: make the login endpoint public to avoid JWT verification

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { ApiBody, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { AllowAny } from './allow-any.decorator';
 
 @ApiTags('authentication')
 @Controller('auth')
@@ -9,14 +10,18 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @ApiSecurity('')
+  @AllowAny()
   @UseGuards(LocalAuthGuard)
   @Post('login')
   @ApiBody({
     schema: {
       type: 'object',
       properties: {
-        email: { type: 'string' },
-        password: { type: 'string' },
+        email: {
+          type: 'string',
+          description: 'Email válido de um usuário ativo.',
+        },
+        password: { type: 'string', description: 'Senha do usuário.' },
       },
     },
   })
