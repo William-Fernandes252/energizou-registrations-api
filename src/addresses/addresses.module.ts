d46c3461ea8097b48ff8a4fb492671464ea590ee
William Fernandes Dias <william.winchester1967@gmail.com>
2023-12-07 17:45:07 -0300

    fix: constraint provider import

--- src/addresses/addresses.module.ts
+++ src/addresses/addresses.module.ts
@@ -4,7 +4,7 @@ import { AddressesController } from './addresses.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Address } from './entities/address.entity';
 import { CaslModule } from 'src/casl/casl.module';
-import AddressAlreadyExistsConstraint from './validators/constraints';
+import { AddressAlreadyExistsConstraint } from './validators/constraints';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Address]), CaslModule],
